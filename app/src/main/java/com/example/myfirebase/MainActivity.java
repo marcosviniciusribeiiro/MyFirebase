1fddd25034868f7b4ec5767ade419877f2d362a0
MarcosVinicius <Marcosvini.rg@gmail.com>
2026-05-19 11:09:54 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/myfirebase/MainActivity.java
+++ app/src/main/java/com/example/myfirebase/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     public EditText campo_email;
     public EditText campo_senha;
+    public EditText novo_email;
+    public EditText nova_senha;
+    public  EditText confirmar_senha;
     public Button btn_login;
     public Button btn_registrar;
     public  Button btn_cadastrar;
@@ -41,9 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         campo_email = findViewById(R.id.id_edit_email);
+        novo_email = findViewById(R.id.id_novo_email);
         campo_senha = findViewById(R.id.id_edit_senha);
+        nova_senha = findViewById(R.id.id_nova_senha);
+        confirmar_senha = findViewById(R.id.id_confirmar_senha);
         btn_login = findViewById(R.id.id_btn_login);
         btn_registrar = findViewById(R.id.id_btn_registrar);
+        btn_cadastrar = findViewById(R.id.id_btn_cadastrar);
 
         btn_login.setOnClickListener(view ->
                 validar()
@@ -56,6 +63,16 @@ public class MainActivity extends AppCompatActivity {
               dialog.setContentView(R.layout.dialog_registro);
               dialog.show();
             }});
+//        btn_cadastrar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(nova_senha.equals(confirmar_senha)){
+//                    Toast.makeText(MainActivity.this, "ok", Toast.LENGTH_LONG).show();
+//                }else {
+//                    Toast.makeText(MainActivity.this, "erro", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
     }
 
     private void validar() {
@@ -72,17 +89,18 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    private void registrar(){
-        String email = campo_email.getText().toString();
-        String password = campo_senha.getText().toString();
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(MainActivity.this,"usuário registrado com sucesso", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(MainActivity.this,"não foi possível -registrar o usuário", Toast.LENGTH_LONG).show();
-                    }
-                });
-    }
+//    private void registrar(){
+//        String email = novo_email.getText().toString();
+//
+//        String password = nova_senha.getText().toString();
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, task -> {
+//                    if (task.isSuccessful()) {
+//                        Toast.makeText(MainActivity.this,"usuário registrado com sucesso", Toast.LENGTH_LONG).show();
+//                    } else {
+//                        Toast.makeText(MainActivity.this,"não foi possível -registrar o usuário", Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//    }
 
 }
