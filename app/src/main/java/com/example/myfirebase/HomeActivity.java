d1acf7407f4f537118349898b4da40e8b8332f70
MarcosVinicius <Marcosvini.rg@gmail.com>
2026-05-26 09:40:49 -0300

    Update HomeActivity.java

--- app/src/main/java/com/example/myfirebase/HomeActivity.java
+++ app/src/main/java/com/example/myfirebase/HomeActivity.java
@@ -1,6 +1,12 @@
 package com.example.myfirebase;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+    EditText campo_tarefa;
+    Button btn_add;
+    ListView listView;
+    ArrayAdapter<Tarefa> arrayAdapter;
+    ArrayList<Tarefa> arrayList;
+    Tarefa tarefa;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("tarefa");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,26 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        campo_tarefa = findViewById(R.id.id_campo_tarefa);
+        btn_add = findViewById(R.id.id_btn_add);
+        listView = findViewById(R.id.id_list);
+        arrayList = new ArrayList<Tarefa>();
+        arrayAdapter = new ArrayAdapter<Tarefa>(HomeActivity.this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        btn_add.setOnClickListener(view -> salvarTarefa());
+    }
+
+    private void salvarTarefa() {
+        String tarefa = campo_tarefa.getText().toString();
+
+        if(tarefa.isEmpty()){
+            Toast.makeText(HomeActivity.this, "Preencha a tarefa", Toast.LENGTH_LONG).show();
+        }
+
+        Tarefa minhaTarefa = new Tarefa();
+        minhaTarefa.setTitulo(tarefa);
+        myRef.setValue(minhaTarefa);
+        arrayList.add(minhaTarefa);
     }
 }
